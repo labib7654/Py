06e15020e88bfec21a9f2b4185f56bcd753ed94a
labib7654 <labibradaan@gmail.com>
2026-05-16 17:30:57 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  BOT_TOKEN:    process.env.BOT_TOKEN    || '',
+  DEVELOPER_ID: Number(process.env.DEVELOPER_ID || '0'),
+  PORT:         Number(process.env.PORT          || 3000),
+};
