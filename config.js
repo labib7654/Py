d7f9c4cc004085a2f0ac8fc5d9adb02b0417f823
labib7654 <labibradaan@gmail.com>
2026-05-16 17:25:02 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  BOT_TOKEN:    process.env.BOT_TOKEN    || '',
-  DEVELOPER_ID: Number(process.env.DEVELOPER_ID || '0'),
-  PORT:         Number(process.env.PORT          || 3000),
-};
