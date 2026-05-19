a049d3186b4f557f9ed42af9322f2af65b6eb3d9
labib7654 <labibradaan@gmail.com>
2026-05-19 05:05:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
 const { BOT_TOKEN } = require('./config');
 const db = require('./db');
 const {
@@ -16,8 +17,13 @@ if (!BOT_TOKEN) {
 }
 
 const bot = new Telegraf(BOT_TOKEN);
+const app = express();
+const PORT = Number(process.env.PORT || 3000);
 
 applyTelegramContentProtection(bot);
+
+app.get('/', (_req, res) => res.status(200).send('ok'));
+app.get('/healthz', (_req, res) => res.status(200).json({ ok: true }));
 
 setupGroupHandlers(bot);
 setupOwnerHandlers(bot);
@@ -31,6 +37,9 @@ bot.catch((err, ctx) => {
 });
 
 async function start() {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`HTTP server listening on ${PORT}`);
+  });
   await bot.launch();
   const stats = db.getStats();
   console.log(`Bot launched. groups=${stats.totalGroups} users=${stats.totalUsers}`);
@@ -45,5 +54,13 @@ if (require.main === module) {
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
+});
 
 module.exports = { bot, start };
